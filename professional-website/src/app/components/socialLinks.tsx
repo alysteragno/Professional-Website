3bf41ff5c8b0490f73f6a59fdcb82076e8866772
Alyster <alysteragno@gmail.com>
2025-05-24 19:34:26 +0800

    feat(nav): added github logo and flexbox

---
+++ professional-website/src/app/components/socialLinks.tsx
@@ -0,0 +1,39 @@
+'use client';
+import Image from "next/image";
+import Link from "next/link";
+
+const socialLinks = [
+    {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/alyster-marasigan-9314692ba/",    
+        icon: "/linkedLogo.svg",    
+    }, 
+    {
+        name: "Github",
+        url: "https://github.com/alysteragno",    
+        icon: "/githubLogo.svg",    
+    }, 
+
+];
+export default function SocialLinks() {
+  return (
+    <>
+    {socialLinks.map(({ name, url, icon }) => (
+         <Link
+            key={name}
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label={`Visit my ${name} profile`}
+          >
+            <Image
+              src={icon}
+              width={30}
+              height={30}
+              alt={`${name} Logo`}
+            />
+          </Link>
+    ))}
+    </>
+  );
+}
