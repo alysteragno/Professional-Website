e9fdb3fb13e146d995e86554163707132f08ac6b
Alyster <alysteragno@gmail.com>
2025-05-24 19:39:21 +0800

    feat(header): Implemented the lightmode component inside the header component

--- professional-website/src/app/components/header.tsx
+++ professional-website/src/app/components/header.tsx
@@ -1,25 +1,18 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import SocialLinks from './socialLinks';
+import Lightmode from './lightmode';
+
 
 export default function Header() {
   return (
     <header className='justify-between flex items-center p-4'>
       <div>
-        <div>
-          <h3>Alyster A.</h3>
-        </div>
+        <h3>Alyster A.</h3>
       </div>
 
-      <div className="socialsContainer">
-        <Link href="https://www.linkedin.com/in/alyster-marasigan-9314692ba/" target="_blank">
-          <Image
-            src="/linkedLogo.svg"
-            width={100}
-            height={100}
-            alt="LinkedIn Logo"
-          />
-        </Link>
-      </div>
+      <nav className='flex items-center gap-3'>
+        <SocialLinks />
+        <Lightmode/>
+      </nav>
     </header>
   );
 }
