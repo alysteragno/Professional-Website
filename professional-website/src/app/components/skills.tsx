4a494f1d717ed16390eb41251e1ade712cd5115e
Alyster <alysteragno@gmail.com>
2025-06-01 23:04:56 +0800

    fix(skills): moved skill name beside skills logo

--- professional-website/src/app/components/skills.tsx
+++ professional-website/src/app/components/skills.tsx
@@ -56,29 +56,29 @@ const skills = [
 
 export default function Skills() {
   return (
-    <div className="p-6">
-      <h2 className="text-2xl font-bold mb-4">Skills</h2>
-      <div></div>
-      <div className="flex flex-col gap-3">
-        {skills.map((skill) => (
-          <Link key={skill.name} href={skill.url} target="_blank">
-            <div className="">
-              <Image
-                src={`/skills/${skill.image}`} 
-                alt={skill.name}
-                width={40}
-                height={40}
-                className='hover:scale-115 hover:transition-transform duration-200'
-              />
+    <section className="p-6">
+  <h2 className="text-2xl font-bold mb-4">Skills</h2>
+  <div className="flex ">
+    <div>
+      {skills.map((skill) => (
+        <Link key={skill.name} href={skill.url} target="_blank">
+          <div className="flex items-center mb-4">
+            <Image
+              src={`/skills/${skill.image}`} 
+              alt={skill.name}
+              width={40}
+              height={40}
+              className="hover:scale-115 hover:transition-transform duration-200"
+            />
+            <div className='ml-4'>
+              <span className="text-sm gap-5">{skill.name}</span>
             </div>
-            <div>
-                <span className="mt-2 text-sm">{skill.name}</span>
-            </div>
-
-            
-          </Link>
-        ))}
-      </div>
+          </div>
+        </Link>
+      ))}
     </div>
+  </div>
+</section>
+
   );
 }
