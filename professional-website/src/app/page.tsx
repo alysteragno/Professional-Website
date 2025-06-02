fee5de7092f034cf957ca39689492629c8e3dd46
Alyster <alysteragno@gmail.com>
2025-06-02 18:38:20 +0800

    feat(page.tsx): added and imported component skills

--- professional-website/src/app/page.tsx
+++ professional-website/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./components/hero"
+import Skills from "./components/skills"
   
 export default function Home() {
   return (
     <div className="mx-3">
       <Hero/>
+      <Skills/>
     </div>
   );
 }
