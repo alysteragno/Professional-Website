1efafcef8fd93fba536412eef014f08076468af3
Alyster <alysteragno@gmail.com>
2025-06-09 21:25:38 +0800

    merge: hotfix/dev into dev

--- professional-website/src/app/page.tsx
+++ professional-website/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/hero"
 import Skills from "./components/skills/skills"
+import Carousel from "./components/carousel"
 
 export default function Home() {
   return (
     <div className="mx-3">
       <Hero/>
       <Skills/>
+      <Carousel/>
     </div>
   );
 }
