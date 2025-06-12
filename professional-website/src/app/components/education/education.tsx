7898035aadd6fed2c8ae7f18296e0c39f9f40c6a
Alyster <alysteragno@gmail.com>
2025-06-12 18:01:12 +0800

    feat: implemented responsiveness on education component lg breakpoint

--- professional-website/src/app/components/education/education.tsx
+++ professional-website/src/app/components/education/education.tsx
@@ -17,7 +17,7 @@ export default function Education() {
                 <Line />
             </div>
 
-            <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-col justify-center items-center lg:inline-block">
                 {schools.map((school) => (
                     <Link
                         prefetch={true}
@@ -32,11 +32,11 @@ export default function Education() {
                             width={100}
                             height={100}
                             alt={school.name}
-                            className="m-auto mt-5"
+                            className="m-auto mt-5 lg:m-3 lg:w-[130px]"
                         />
                         <div className="text-center mt-2">
-                            <h2 className="bold text-primary text-xl">{school.name}</h2>
-                            <h3 className="bold text-primary">{school.name2}</h3>
+                            <h2 className="bold text-primary text-xl lg:text-start lg:ml-3">{school.name}</h2>
+                            <h3 className="bold text-primary lg:text-start lg:ml-3">{school.name2}</h3>
 
                             <a
                                 href={school.fieldUrl}
@@ -44,11 +44,11 @@ export default function Education() {
                                 rel="noopener noreferrer"
                                 className="hover:underline"
                             >
-                                <h2 className="bold text-secondary text-lg">{school.field}</h2>
+                                <h2 className="bold text-secondary text-lg lg:text-start lg:ml-3">{school.field}</h2>
                             </a>
 
-                            <h2 className="bold text-tertiary text-lg">{school.major}</h2>
-                            <h4 className="text-primary">{school.year}</h4>
+                            <h2 className="bold text-tertiary text-lg lg:text-start lg:ml-3">{school.major}</h2>
+                            <h4 className="text-primary lg:text-start lg:ml-3 lg:mb-20">{school.year}</h4>
                         </div>
                     </Link>
                 ))}
