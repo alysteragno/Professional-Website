98cda580dfbc8157abc981b59b3a3912710ec61b
Alyster <alysteragno@gmail.com>
2025-06-12 23:37:46 +0800

    feat(contact): implemented max and min character allowed and implemented restrict input of integers and special chars

--- professional-website/src/app/components/contactme/contact.tsx
+++ professional-website/src/app/components/contactme/contact.tsx
@@ -8,47 +8,58 @@ export default function Contact() {
                 <ComponentName name = "Contact me !" />
             </div>
             <div className="flex flex-col items-center">
-                <div>
-                    <div className="flex justify-between  w-[29em] lg:w-[35em]">
-                        <div className='flex justify-between w-full'>
-                            <ContactInput 
-                                id="firstname"
-                                placeholder="First Name"
-                                className="w-[14em] lg:w-[17em]"
-                            />
-                            <ContactInput 
-                                id="lastname"
-                                placeholder="Last Name"
-                                className="w-[14em] lg:w-[17em]"
+                <form>
+                    <div>
+                        <div className="flex justify-between  w-[29em] lg:w-[35em]">
+                            <div className='flex justify-between w-full'>
+                                <ContactInput 
+                                    id="firstname"
+                                    placeholder="First Name"
+                                    className="w-[14em] lg:w-[17em]"
+                                    required={true}
+                                    minLength={3}
+                                    pattern="^[^\d]*$"
+                                />
+                                <ContactInput 
+                                    id="lastname"
+                                    placeholder="Last Name"
+                                    className="w-[14em] lg:w-[17em]"
+                                    required={true}
+                                    minLength={3}
+                                    pattern="^[^\d]*$"
+                                />
+                            </div>
+                        </div>
+
+                        {/* Email Input */}
+                        <div>
+                            <ContactInput
+                                id="Email"
+                                placeholder='Email'
+                                className='w-full'
+                                required={true}
+                                minLength={10}
                             />
                         </div>
-                    </div>
-
-                    {/* Email Input */}
-                    <div>
-                        <ContactInput
-                            id="Email"
-                            placeholder='Email'
-                            className='w-full'
+                        <div className="w-full">
+                        <textarea
+                            id="message"
+                            placeholder="Your Message"
+                            className="w-full h-32 p-3 border-2 border-[#8892B0] rounded-md focus:outline-none focus:ring-2 focus:ring-white-500 resize-none"
+                            required={true}
                         />
-                    </div>
-                    <div className="w-full">
-                    <textarea
-                        id="message"
-                        placeholder="Your Message"
-                        className="w-full h-32 p-3 border-2 border-[#8892B0] rounded-md focus:outline-none focus:ring-2 focus:ring-white-500 resize-none"
-                    />
-                    </div>
+                        </div>
 
-                    <div className='items-center flex justify-center my-5'>
-                        <button 
-                            className='border-2 border-[#8892B0] rounded-xl p-4 w-full hover:cursor-pointer hover:bg-[#1B2D47]
-                                       lg:w-[12em] lg:p-3'
-                        >
-                            <h4 className='font-bold text-xl lg:text-lg'>Submit</h4>
-                        </button>
-                    </div>          
-                </div>
+                        <div className='items-center flex justify-center my-5'>
+                            <button 
+                                className='border-2 border-[#8892B0] rounded-xl p-4 w-full hover:cursor-pointer hover:bg-[#1B2D47]
+                                        lg:w-[12em] lg:p-3'
+                            >
+                                <h4 className='font-bold text-xl lg:text-lg'>Submit</h4>
+                            </button>
+                        </div>          
+                    </div>
+              </form>
             </div>
       </div>
     );
--- professional-website/src/app/components/contactme/contactInput.tsx
+++ professional-website/src/app/components/contactme/contactInput.tsx
@@ -3,24 +3,23 @@ type Props = {
     id: string;
     placeholder: string;
     className?: string;
+    required?: boolean;
+    minLength?: number;
+    pattern?: string;
 };
 
-export default function ContactInput( { id, placeholder, className } : Props ) {
+export default function ContactInput( { id, placeholder, className, required, minLength, pattern } : Props ) {
     return (
-        <div>
-            <form 
-                action=""
-                className=""
-            >
-            <input  
-                type="text" 
-                id={id}
-                name={id}
-                placeholder={placeholder}
-                className={`border-2 border-[#8892B0] rounded-md p-3 mb-4 focus:outline-none focus:ring-1 focus:ring-white-500 resize-none ${className || '' }`}
-            />
-            </form>
-         
-        </div>
+        <input  
+            type="text" 
+            id={id}
+            name={id}
+            placeholder={placeholder}
+            className={`border-2 border-[#8892B0] rounded-md p-3 mb-4 focus:outline-none focus:ring-1 focus:ring-white-500 resize-none ${className || '' }`}
+            required={required}
+            minLength={minLength}
+            autoComplete="on"
+            pattern={pattern}
+        />
     )
 }   
