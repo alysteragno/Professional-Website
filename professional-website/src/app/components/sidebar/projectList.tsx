23c0d4a800aa864f7f839c3ca6c9e3062d70f610
Alyster <alysteragno@gmail.com>
2025-06-15 20:18:49 +0800

    feat(sidebar): add ProjArray component for displaying project links with hover functionality

---
+++ professional-website/src/app/components/sidebar/projectList.tsx
@@ -0,0 +1,51 @@
+'use client';
+import Link from 'next/link';
+
+export type Proj = {
+  name: string;
+  id: string;
+  video: string | null;
+  route: string;
+  onHover?: (video: string | null) => void;
+};
+
+export const Projects: Proj[] = [
+  {
+    id: "1",
+    name: "Bank System",
+    video: "/video/BankSystem.mp4",
+    route: "https://www.youtube.com/watch?v=xvFZjo5PgG0",
+  },
+  {
+    id: "2",
+    name: "Weather App",
+    video: "/video/BankSystem.mp4",
+    route: "https://www.youtube.com/watch?v=xvFZjo5PgG0",
+  },
+];
+
+export default function ProjArray({ id, name, video, route, onHover }: Proj) {
+
+  return (
+    <nav className="w-full">
+      <div className="flex">
+      <ul>
+        <li
+            className="flex flex-col justify-center  hover:scale-105 transition-all rounded-sm"
+            onMouseEnter={() => onHover?.(video)}
+            onMouseLeave={() => onHover?.(null)}
+          >
+          <Link
+            href={route}
+            target="_blank"
+            rel="noopener noreferrer"
+            prefetch={true}
+          >
+            <h2 className='text-primary'>{name}</h2>
+          </Link>
+        </li>
+      </ul>
+      </div>
+    </nav>
+  );
+}
