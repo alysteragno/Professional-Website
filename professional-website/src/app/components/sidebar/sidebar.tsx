5c218cc67ed058b015fcc0d43b33894881647da1
Alyster <alysteragno@gmail.com>
2025-06-15 20:12:33 +0800

    feat(sidebar): created folder sidebar with sidebar.tsx component inside

---
+++ professional-website/src/app/components/sidebar/sidebar.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { useState } from 'react';
+import ProjArray from './projectList';
+import { Projects } from './projectList';
+import VideoPlayer from './projectHover'; 
+import AnimatedPopup from './AnimatedPopup';
+
+export default function Sidebae() {
+
+    return(
+        
+    )
+}
