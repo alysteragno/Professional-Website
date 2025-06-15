ddfb18f38eccb93d56adc7153f12b90f436e70d4
Alyster <alysteragno@gmail.com>
2025-06-15 20:17:31 +0800

    fix(sidebar): correct function name and implement video hover functionality

--- professional-website/src/app/components/sidebar/sidebar.tsx
+++ professional-website/src/app/components/sidebar/sidebar.tsx
@@ -6,9 +6,39 @@ import { Projects } from './projectList';
 import VideoPlayer from './projectHover'; 
 import AnimatedPopup from './AnimatedPopup';
 
-export default function Sidebae() {
+export default function Sidebar() {
 
-    return(
-        
-    )
+  const [hoveredVideo, setHoveredVideo] = useState<string | null>(null);
+
+  const handleHover = (video: string | null) => {
+    setHoveredVideo(video);
+  };
+  return (
+    <div className="hidden lg:block w-[15em] fixed right-0 z-100 bg-[#0a192f] h-full">
+      <div> 
+        <div className='inline-block w-20'>
+          <h2 className="mt-25 border-b-1">Portfolio</h2>
+        </div>
+        {Projects.map((project) => (
+          <div key={project.id} className='mt-4'>
+            
+            <ProjArray
+              id={project.id}
+              name={project.name}
+              video={project.video}
+              route={project.route}
+              onHover={handleHover}
+            />
+          </div>
+        ))}
+
+        {/* The div of the video */}
+        {hoveredVideo && (
+        <AnimatedPopup show={!!hoveredVideo}>
+          <VideoPlayer src={hoveredVideo!} />
+        </AnimatedPopup>
+        )}
+        </div>
+      </div>
+  );
 }
